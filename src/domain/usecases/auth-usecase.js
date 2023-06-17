853ad3b6f96b06115d1db882703803087b99889a
Eduardo Rodrigues <contatoeduardofelipe@hotmail.com>
2023-06-17 11:41:47 -0300

    Refactor code to simplify AuthUseCase

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -13,11 +13,24 @@ module.exports = class AuthUseCase {
     if (!this.loadUserByEmailRepository) throw new MissingParamError('loadUserByEmailRepository')
     if (!this.loadUserByEmailRepository.load) throw new InvalidParamError('loadUserByEmailRepository')
 
+    /*
+      Opção 1 de fluxo de validações
+      const user = await this.loadUserByEmailRepository.load(email)
+      if (!user) return null
+      const isValidPassword = await this.encrypter.compare(password, user.hashedPassword)
+      if (!isValidPassword) return null
+      const accessToken = await this.tokenGenerator.generate(user.id)
+      return accessToken
+    */
+
     const user = await this.loadUserByEmailRepository.load(email)
-    if (!user) return null
-    const isValidPassword = await this.encrypter.compare(password, user.hashedPassword)
-    if (!isValidPassword) return null
-    const accessToken = await this.tokenGenerator.generate(user.id)
-    return accessToken
+    if (user) {
+      const isValidPassword = await this.encrypter.compare(password, user.hashedPassword)
+      if (isValidPassword) {
+        const accessToken = await this.tokenGenerator.generate(user.id)
+        return accessToken
+      }
+    }
+    return null
   }
 }
