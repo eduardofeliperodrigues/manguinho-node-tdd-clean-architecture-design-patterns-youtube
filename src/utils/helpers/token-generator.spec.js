a92972ef90416bc322b922e23a94ba60754a6143
Eduardo Rodrigues <contatoeduardofelipe@hotmail.com>
2023-06-22 22:39:06 -0300

    Ensure TokenGenerator return a token if JWT returns token

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,13 +1,23 @@
+const jwt = require('jsonwebtoken')
+
 class TokenGenerator {
   generate (id) {
-    return null
+    const token = jwt.sign(id, 'secret')
+    return token
   }
 }
 
 describe('Token Generator', () => {
   test('Should return null if JWT returns null', async () => {
     const sut = new TokenGenerator()
+    jwt.token = null
     const token = await sut.generate('any_id')
-    expect(token).toBe(null)
+    expect(token).toBeNull()
+  })
+
+  test('Should return a token if JWT returns tokne', async () => {
+    const sut = new TokenGenerator()
+    const token = await sut.generate('any_id')
+    expect(token).toBe(jwt.token)
   })
 })
