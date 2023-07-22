1c6108d2b3c8f31f32e7656bbe05825cf12e11f9
Eduardo Rodrigues <contatoeduardofelipe@hotmail.com>
2023-07-22 14:37:38 -0300

    move loginRouter composition to its own file

---
+++ src/main/composers/login-router-composer.js
@@ -0,0 +1,26 @@
+const LoginRouter = require('../../presentation/routers/login-router')
+const AuthUseCase = require('../../domain/usecases/auth-usecase')
+const LoadUserByEmailRepository = require('../../infra/repositories/load-user-by-email-repository')
+const UpdateAccessTokenRepository = require('../../infra/repositories/update-access-token-repository')
+const EmailValidator = require('../../utils/helpers/email-validator')
+const Encrypter = require('../../utils/helpers/encrypter')
+const TokenGenerator = require('../../utils/helpers/token-generator')
+const env = require('../config/env')
+
+const emailValidator = new EmailValidator()
+const encrypter = new Encrypter()
+const tokenGenerator = new TokenGenerator(env.tokenSecret)
+const loadUserByEmailRepository = new LoadUserByEmailRepository()
+const updateAccessTokenRepository = new UpdateAccessTokenRepository()
+const authUseCase = new AuthUseCase({
+  encrypter,
+  tokenGenerator,
+  loadUserByEmailRepository,
+  updateAccessTokenRepository
+})
+const loginRouter = new LoginRouter({
+  authUseCase,
+  emailValidator
+})
+
+module.exports = loginRouter
